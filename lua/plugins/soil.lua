22285afc8fe967cb44ff9b0d0e73342c636bc00f
The-Arkitekt <mr.davidcrumley@gmail.com>
2024-06-08 16:10:55 +0000

    added debugging, removed redundant key mapping

--- lua/plugins/soil.lua
+++ lua/plugins/soil.lua
@@ -5,8 +5,5 @@ return
   ft   = "plantuml",
   dependencies = {
     "javiorfo/nvim-nyctophilia"
-  },
-  config = function()
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end
+  }
 }
