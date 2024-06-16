927376825b2cf7698f034d68932d6934a97de49e
The-Arkitekt <mr.davidcrumley@gmail.com>
2024-06-16 17:31:46 +0000

    updatedd soil configuration

--- lua/plugins/soil.lua
+++ lua/plugins/soil.lua
@@ -5,5 +5,17 @@ return
   ft   = "plantuml",
   dependencies = {
     "javiorfo/nvim-nyctophilia"
-  }
+  },
+  config = function()
+    require('soil').setup {
+      image = {
+        darkmode = false,
+        format   = "png",
+
+        execute_to_open = function(img)
+          return "nsxiv -b " .. img
+        end
+      }
+    }
+  end
 }
